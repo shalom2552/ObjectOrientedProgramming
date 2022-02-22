08107af8b03c17475d1526c493b045919de5f393
Shalom <64192029+shalom2552@users.noreply.github.com>
2022-02-22 16:04:38 +0200

    Delete unnecessary files

--- HW3/src (copy)/AbstractInvertedIndexFactory.java
+++
@@ -1,6 +0,0 @@
-
-public abstract class AbstractInvertedIndexFactory {
-
-    public abstract AbstractInvertedIndex createInvertedIndex();
-
-}
--- HW3/src (copy)/CaseSensitiveIndex.java
+++
@@ -1,9 +0,0 @@
-import java.io.File;
-import java.util.TreeSet;
-
-public class CaseSensitiveIndex extends AbstractInvertedIndex{
-
-    public CaseSensitiveIndex(){ }
-    // TODO this should be private. its a single tone!
-
-}
